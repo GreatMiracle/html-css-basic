c1558c0ab8877795ca3a28ffbe66aaf457ed87f8
kien.nt <kiennguyen16998@gmail.com>
2023-10-30 22:33:51 +0700

    update js for button login and register

--- html-first/shoppe-app/assets/javascript/main.js
+++ html-first/shoppe-app/assets/javascript/main.js
@@ -12,3 +12,56 @@ togglePassword.addEventListener("click", function () {
         togglePassword.classList.add("fa-eye");
     }
 });
+
+
+const modal = document.getElementById("model");
+const modalOverlay = document.getElementById("modal__overlay");
+const registerForm = document.getElementById("auth-form-register");
+const loginForm = document.getElementById("auth-form-login");
+const openRegisterButton = document.getElementById("hd-nav__r-register");
+const openLoginButton = document.getElementById("hd-nav__r-login");
+
+function openRegisterFunc() {
+
+    modal.classList.add("show-popup-regisster-login");
+    modal.classList.remove("hidden-popup-regisster-login");
+
+    registerForm.classList.add("show-popup-regisster-login");
+    registerForm.classList.remove("hidden-popup-regisster-login");
+
+    loginForm.classList.add("hidden-popup-regisster-login");
+    loginForm.classList.remove("show-popup-regisster-login");
+}
+
+function openLoginFunc() {
+
+    modal.classList.add("show-popup-regisster-login");
+    modal.classList.remove("hidden-popup-regisster-login");
+
+    registerForm.classList.add("hidden-popup-regisster-login");
+    registerForm.classList.remove("show-popup-regisster-login");
+
+    loginForm.classList.add("show-popup-regisster-login");
+    loginForm.classList.remove("hidden-popup-regisster-login");
+}
+
+function modalOverlayFunc() {
+    modal.classList.add("hidden-popup-regisster-login");
+    modal.classList.remove("show-popup-regisster-login");
+}
+
+modalOverlay.addEventListener("click", modalOverlayFunc);
+openRegisterButton.addEventListener("click", openRegisterFunc);
+openLoginButton.addEventListener("click", openLoginFunc);
+
+
+
+const switchToLogin = document.getElementById("auth-form__switch-btn-toLogin");
+const switchToRegister = document.getElementById("auth-form__switch-btn-toRegister");
+const noneRegister = document.getElementById("auth-form__btn-register-back");
+const noneLogin = document.getElementById("auth-form__btn-login-back");
+
+switchToLogin.addEventListener("click", openLoginFunc);
+switchToRegister.addEventListener("click", openRegisterFunc);
+noneRegister.addEventListener("click", modalOverlayFunc);
+noneLogin.addEventListener("click", modalOverlayFunc);
