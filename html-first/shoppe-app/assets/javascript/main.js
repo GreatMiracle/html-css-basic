e7ad010cfe402a2c0d0463252982164cdfdaea34
Thaoho <thao.ho@sotatek.com>
2023-10-30 17:20:26 +0700

    end css popup login register

---
+++ html-first/shoppe-app/assets/javascript/main.js
@@ -0,0 +1,14 @@
+const passwordInput = document.getElementById("auth-form__password");
+const togglePassword = document.getElementById("auth-form__togglePassword");
+
+togglePassword.addEventListener("click", function () {
+    if (passwordInput.type === "password") {
+        passwordInput.type = "text";
+        togglePassword.classList.remove("fa-eye");
+        togglePassword.classList.add("fa-eye-slash");
+    } else {
+        passwordInput.type = "password";
+        togglePassword.classList.remove("fa-eye-slash");
+        togglePassword.classList.add("fa-eye");
+    }
+});
